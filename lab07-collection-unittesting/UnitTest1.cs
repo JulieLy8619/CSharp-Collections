9a9efce270ac14ac972c45bbdf0f6aa188dd3f6d
juliely8619 <july8619@yahoo.com>
2019-01-19 16:36:35 -0800

    program part done

---
+++ lab07-collection-unittesting/UnitTest1.cs
@@ -0,0 +1,22 @@
+using System;
+using Xunit;
+
+namespace lab07_collection_unittesting
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //all my methods were return type VOID
+
+
+        }
+    }
+}
+
+
+//Add a card to your deck
+//Getter/Setters of your properties from your Card class
+//Remove a card from your deck that exists
+//Cannot remove a card from your deck that does not exist
